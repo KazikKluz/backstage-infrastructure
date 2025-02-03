f198fccf6ffa3f457ec6d0dd2651ecd6e9f2cdc4
KazikKluz <kazik.kluz@gmail.com>
2025-02-03 17:05:25 +0000

    azure

--- azure-vm-pulumi/__main__.py
+++ azure-vm-pulumi/__main__.py
@@ -2,28 +2,32 @@ import pulumi
 import pulumi_azure as azure
 import os
 
+
+public_key = os.environ.get("ID_RSA_PUB")
+instance_location = os.environ.get("INSTANCE_LOCATION")
+resource_group_name = os.environ.get("RESOURCE_GROUP_NAME")
+instance_name = os.environ.get("INSTANCE_NAME")
 # Read existing SSH public key from .ssh folder
-with open(os.path.expanduser('~/.ssh/id_rsa.pub'), 'r') as f:
-    ssh_public_key_data = f.read().strip()
 
 # Create an SSH key resource in Azure
 ssh_public_key = azure.compute.SshPublicKey(
     "exampleSshPublicKey",
-    resource_group_name="example-resource-group",
-    location="WestUS",
-    public_key=ssh_public_key_data
+    resource_group_name=resource_group_name,
+    location=instance_location,
+    public_key=public_key
 )
 
 # Create a network interface for the VM
 network_interface = azure.network.NetworkInterface(
     "exampleNetworkInterface",
-    resource_group_name="example-resource-group",
+    resource_group_name=resource_group_name,
+    location=instance_location,
     ip_configurations=[azure.network.NetworkInterfaceIpConfigurationArgs(
         name="internal",
         subnet=azure.network.VirtualNetworkSubnetArgs(
             name="internal",
-            resource_group_name="example-resource-group",
-            virtual_network_name="example-vnet"
+            resource_group_name=resource_group_name,
+            virtual_network_name=f"{instance_name}-vnet"
         ),
         private_ip_address_allocation="Dynamic"
     )]
@@ -31,10 +35,10 @@ network_interface = azure.network.NetworkInterface(
 
 # Create a Linux VM
 vm = azure.compute.LinuxVirtualMachine(
-    "exampleLinuxVM",
-    location="WestUS",
-    resource_group_name="example-resource-group",
-    size="Standard_B2s",
+    f"{instance_name}-vm",
+    location=instance_location,
+    resource_group_name=resource_group_name,
+    size="Standard_B1s",
     admin_username="adminuser",
     network_interface_ids=[network_interface.id],
     os_disk=azure.compute.LinuxVirtualMachineOsDiskArgs(
